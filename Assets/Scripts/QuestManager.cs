60f2f7e22adb895542ca80c51ac7a88a5bb1ba36
BL4CK DoG <xoxohoon01@gmail.com>
2024-10-23 12:09:31 +0900

    Q1 - 구현사항 2

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -5,7 +5,26 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     private static QuestManager instance;
-    public static QuestManager Instance;
+    public static QuestManager Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                QuestManager newQuestManager = FindObjectOfType<QuestManager>();
 
+                if (newQuestManager != null)
+                {
+                    instance = newQuestManager;
+                }
+                else
+                {
+                    GameObject newGameObject = new GameObject();
+                    instance = newGameObject.AddComponent<QuestManager>();
+                }
+            }
+            return instance; 
+        }
+    }
 
 }
