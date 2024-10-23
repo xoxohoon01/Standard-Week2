67319492c8c96f97c815911b7a727d8d4b72e6ae
BL4CK DoG <xoxohoon01@gmail.com>
2024-10-23 12:04:41 +0900

    Q1 - 구현사항 1

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    private static QuestManager instance;
+    public static QuestManager Instance;
+
+
+}
