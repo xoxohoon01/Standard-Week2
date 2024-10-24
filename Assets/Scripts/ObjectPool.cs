be15469323c7fc37dc75475b2f4a5a892e63c894
BL4CK DoG <xoxohoon01@gmail.com>
2024-10-24 13:00:40 +0900

    Q2 - 구현사항 2 수정

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -3,38 +3,41 @@ using System.Collections.Generic;
 
 public class ObjectPool : MonoBehaviour
 {
-    public GameObject prefab;
-    private List<GameObject> pool = new List<GameObject>();
-    private Dictionary<string, List<GameObject>> pools = new Dictionary<string, List<GameObject>>();
-    public int poolSize = 300;
+    [System.Serializable]
+    public class Pool
+    {
+        public string name;
+        public GameObject prefab;
+        public int poolSize;
+    }
+
+    public List<Pool> poolList;
+    Dictionary<string, List<GameObject>> poolsDict = new Dictionary<string, List<GameObject>>();
 
     private void Awake()
     {
-        pools.Add("Monster", pool);
     }
 
     void Start()
     {
         // 미리 poolSize만큼 게임오브젝트를 생성한다.
-        for (int i = 0; i < poolSize; i++)
+        foreach (var pool in poolList)
         {
-            GameObject newGameObject = Instantiate(prefab);
-            newGameObject.SetActive(false);
+            List<GameObject> newList = new List<GameObject>();
+            for (int i = 0; i < pool.poolSize; i++)
+            {
+                GameObject newObject = Instantiate(pool.prefab);
+                newObject.SetActive(false);
+                newList.Add(newObject);
+            }
+            poolsDict.Add(pool.name, newList);
         }
     }
+
 
     public GameObject Get()
     {
         // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
-        for (int i = 0; i < poolSize; i++)
-        {
-            if (!pool[i].activeInHierarchy)
-            {
-                pool[i].SetActive(true);
-                return pool[i];
-            }
-        }
-
         return null;
     }
 
